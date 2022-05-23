9504752f101540fe737db1ac32075bd6dccdf2e3
gam-c <stavish@gmail.com>
2022-05-23 15:42:49 -0400

    advance

---
+++ PROYECT 3 BLACKJACK/40. Completing our dice function/index.js
@@ -0,0 +1,11 @@
+// Try to modify the expression so that we get a range from 1 to 6
+
+
+
+
+function rollDice() {
+    let randomNumber = Math.floor( Math.random(0) * 6 ) + 1
+    return randomNumber
+}
+
+console.log( rollDice() )
