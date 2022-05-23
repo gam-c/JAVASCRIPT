d9a08c4aa36977e273954d29826bef0409621d80
gam-c <stavish@gmail.com>
2022-05-23 16:02:16 -0400

    more

--- PROYECT 3 BLACKJACK/42. Complete getRandomNumber function/index.js
+++ PROYECT 3 BLACKJACK/42. Complete getRandomNumber function/index.js
@@ -13,7 +13,14 @@ let cardsEl = document.getElementById("cards-el")
 function getRandomCard() {
     // if 1     -> return 11
     // if 11-13 -> return 10
-    return Math.floor( Math.random()*13 ) + 1
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+       return randomNumer
+    }
 }
 
 function startGame() {
