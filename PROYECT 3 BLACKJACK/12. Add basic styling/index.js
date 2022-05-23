a4517ea7e45e4dcc58f59a48a9d5f4913ea1a66a
gam-c <stavish@gmail.com>
2022-05-23 12:14:52 -0400

    new

--- PROYECT 3 BLACKJACK/12. Add basic styling/index.js
+++ PROYECT 3 BLACKJACK/12. Add basic styling/index.js
@@ -1,11 +1,10 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-// 1. Declare a variable called message and assign its value to an empty string
 let message = ""
-// 2. Reassign the message variable to the string we're logging out
+
 if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
@@ -15,7 +14,5 @@ if (sum <= 20) {
     message = "You're out of the game! 😭"
     isAlive = false
 }
-
-// 3. Log it out!
 
 console.log(message)
